88e2dc38c200a3037c79675feab096a4d7e4a1be
Jon Sharratt <jon@craftship.io>
2018-02-15 22:21:29 +0000

    Made tests integration index pretty

--- tests/integration/index.js
+++ tests/integration/index.js
@@ -1,20 +1,20 @@
-import { createClient } from 'contentful';
+import { createClient } from "contentful";
 
 const client = createClient({
-  space: 'foo-space',
-  accessToken: 'bar-token',
-  host: '127.0.0.1:8090',
-  insecure: true,
+  space: "foo-space",
+  accessToken: "bar-token",
+  host: "127.0.0.1:8090",
+  insecure: true
 });
 
-test('Gets entry', async () => {
-  const response = await client.getEntry('foo');
+test("Gets entry", async () => {
+  const response = await client.getEntry("foo");
 
   expect(response).toMatchSnapshot();
-})
+});
 
-test('Gets all entries', async () => {
+test("Gets all entries", async () => {
   const response = await client.getEntries();
 
   expect(response).toMatchSnapshot();
-})
+});
